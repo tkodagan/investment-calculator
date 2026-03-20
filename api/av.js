81c33224f4de9c02e892d87c59e8f23275ca7bed
tkodagan <63257404+tkodagan@users.noreply.github.com>
2026-03-19 23:45:33 -0400

    Update av.js

--- api/av.js
+++ api/av.js
@@ -1,14 +1,18 @@
-export default async function handler(req, res) {
-  const params = new URLSearchParams(req.query).toString();
-  const response = await fetch(`https://www.alphavantage.co/query?${params}`);
-  const data = await response.json();
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.json(data);
-}
-```
+const https = require('https');
 
-So your repo structure should look like:
-```
-api/
-  av.js
-index.html
+module.exports = function handler(req, res) {
+  const params = new URLSearchParams(req.query).toString();
+  const url = `https://www.alphavantage.co/query?${params}`;
+
+  https.get(url, (apiRes) => {
+    let body = '';
+    apiRes.on('data', chunk => body += chunk);
+    apiRes.on('end', () => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).send(body);
+    });
+  }).on('error', (err) => {
+    res.status(500).json({ error: err.message });
+  });
+};
